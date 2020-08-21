5ceb9818b0866eb0f2e624ebc7a407b9b37c6319
Sourav Kumar <sourav.kumar@outlook.com>
2020-08-21 08:37:38 +0530

    Updates

---
+++ precache-manifest.7954cde1566547c1cc0ec545a0d77a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc9745e031d4f49108a60f06a50defb",
+    "url": "/classwork/index.html"
+  },
+  {
+    "revision": "b16bd4668847e69a3c87",
+    "url": "/classwork/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "556ec21e2fb62854129a",
+    "url": "/classwork/static/js/2.c9afff75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/classwork/static/js/2.c9afff75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b16bd4668847e69a3c87",
+    "url": "/classwork/static/js/main.ebb4f5c0.chunk.js"
+  },
+  {
+    "revision": "b854c544ad5cd921d990",
+    "url": "/classwork/static/js/runtime-main.cfe6c285.js"
+  }
+]);
